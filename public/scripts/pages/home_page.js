6eee106d533bac94a63974097273bae00398e0d9
Calyx404 <127593133+Calyx404@users.noreply.github.com>
2025-10-05 11:47:05 +0800

    FEAT: team carousel

--- public/scripts/pages/home_page.js
+++ public/scripts/pages/home_page.js
@@ -3,7 +3,7 @@ import { Carousel } from "../../components/carousel/carousel.js";
 
 const contextEl = document.getElementById("team-context");
 const carousel = new Carousel("#team-carousel", {
-  autoplay: true,
+  autoplay: false,
   interval: 4000,
 });
 
@@ -15,22 +15,47 @@ carousel.init(team, (activeKey) => {
   }
 
   const headHTML = data.head
-    ? `<div class="head"><h3>${data.head.name}</h3><p>${data.head.bio}</p></div>`
+    ? `<div class="profile">
+      <div class="committee">
+        <img class="committee-icon" src="./assets/icons/${data.icon}" alt="Committee">
+      </div>
+      <div class="head">
+        <q class="head-bio">${data.head.bio}</q>
+        <div class="separator"></div>
+        <div class="head-leader">
+          <div class="leader-icon">
+            <img src="./assets/icons/${data.head.icon}" alt="Head">
+          </div>
+          <div class="leader-description">
+            <h3><a href="mailto:${data.head.email}">${data.head.name}</a></h3>
+            <p>${data.head.role}</p>
+          </div>
+        </div>
+      </div>
+    </div>`
     : "";
 
   const membersHTML = data.members
-    ? `<div class="members">
-        ${data.members
-          .map(
-            (m) => `
-          <div class="member">
-            <img src="./assets/icons/${m.icon}" alt="${m.name}">
-            <h5>${m.name}</h5>
-            <p>${m.role}</p>
+    ? `<div class="account">
+        <h2 class="committee">${activeKey}</h2>
+        <div class="separator"></div>
+        <div class="members">
+          ${data.members
+            .map(
+              (member) => `
+            <div class="member">
+              <div class="member-icon">
+                <img src="./assets/icons/${member.icon}" alt="Member">
+              </div>
+              <div class="member-description">
+                <h3><a href="mailto:${member.email}">${member.name}</a></h3>
+                <p>${member.role}</p>
+              </div>
+            </div>
+          `
+            )
+            .join("")}
           </div>
-        `
-          )
-          .join("")}
       </div>`
     : "";
 
